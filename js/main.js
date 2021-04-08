29192fae1fdea8cf11671149ede49e91a03481c5
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2021-04-08 21:08:56 +0300

    Добавил скроллы и свайперы
    Добавил скроллы и свайперы, в свайперах сверстал все блоки с "видео", настроил свайперы по-отдельности.

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.channel-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next',
+      prevEl: '.channel-button-prev',
+    },
+  
+  });
+
+const swiper2 = new Swiper('.channel-slider-2', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next-2',
+      prevEl: '.channel-button-prev-2',
+    },
+  
+  });
+  const swiper3 = new Swiper('.channel-slider-3', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next-3',
+      prevEl: '.channel-button-prev-3',
+    },
+  
+  });
