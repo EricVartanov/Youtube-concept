e69118d7e31b7d9796456c7aec19641d3664d7f8
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2021-04-09 16:48:14 +0300

    Добавил адаптив и mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,34 +1,87 @@
 const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300:{
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next',
       prevEl: '.channel-button-prev',
     },
-  
   });
 
 const swiper2 = new Swiper('.channel-slider-2', {
     // Optional parameters
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1400:{
+        slidesPerView: 3
+      },
+      1000: {
+        slidesPerView: 2
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next-2',
       prevEl: '.channel-button-prev-2',
     },
-  
   });
   const swiper3 = new Swiper('.channel-slider-3', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300:{
+        slidesPerView: 4
+      },
+      1100: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next-3',
       prevEl: '.channel-button-prev-3',
     },
-  
   });
+  const searchBtn = document.querySelector('.mobile-search');
+  const mobileSearch = document.querySelector('.input-group');
+  searchBtn.addEventListener('click', () => {
+    mobileSearch.classList.toggle('is-open');
+  });
+  
+  if (document.documentElement.scrollWidth <= 640) {
+    swiper.destroy();
+    swiper2.destroy();
+    swiper3.destroy();
+  };
